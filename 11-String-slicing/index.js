1321214ebcf8233432fc3abb13082c9c549ea11d
vi-jigishu <priyanshubhusan07@gmail.com>
2024-09-10 13:01:21 +0530

    Fix string slicing issue in index.js

--- 11-String-slicing/index.js
+++ 11-String-slicing/index.js
@@ -2,7 +2,7 @@
 // string.slice(start, end);
 
 
-const fullName = "broCode";
+const fullName = "Bro Code";
 
 let firstName = fullName.slice(0, 3);
 console.log(firstName)
@@ -10,3 +10,14 @@ console.log(firstName)
 console.log(fullName.slice(3))
 
 console.log(fullName.slice(-1))   //Will begin from the end.
+
+
+console.log("Yout first name is "+ fullName.slice(0, fullName.indexOf(" ")))
+
+
+const email= "Bro1@gmail.com"
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") +1 );
+console.log(username);
+console.log(extension);
